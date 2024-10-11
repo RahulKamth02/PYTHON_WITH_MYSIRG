a8bcbf38c345d64e8545cf3c26ad629e6e526f1a
Rahul <kumarrahul848213@gmail.com>
2024-10-11 13:08:13 +0530

    Add Game Icon future

--- PYTHON_PROJECT/rockPaperSessiors_python_project_01.py
+++ PYTHON_PROJECT/rockPaperSessiors_python_project_01.py
@@ -1,26 +1,30 @@
+# Rule :-Rock win against scissors
+#        scissors win agains papers
+#        paper win agains rock
+#        0 for Rock,2,for paper,3 for scissors 
 
-
+rock = '🪨'
+paper = "📃"
+scissors = '✂'
+gameIcons = [rock, paper, scissors]
 import random
-user_choice=int(input("Enter your choice : Type 0 for Rock, 1 for paper,Type2 for Sessiors :"))
 
-if user_choice>=3 or user_choice<0:
-    print("you enter invalid num ,you loss")
-else :
+user_choice=int(input("Enter you choice : Type 0 for Rock ,1 for pape ,2 for sessiors :"))
+if user_choice>3 or user_choice<0:
+    print("you entered invalid choice,you loss")
+else:
     computer_choice=random.randint(0,2)
-print("Computer_chose:")
-print(computer_choice)
-if computer_choice==user_choice:
-    print("It's a Draw ")
-
-elif computer_choice==0 or user_choice==2:
-    print("You loss")
-
-elif  user_choice==0 or computer_choice==2 :
-    print("You win")
-
-elif computer_choice>user_choice:
-    print("You loss")
-    
-elif user_choice>computer_choice:
-    print("You win")
-
+    print("computer chose :")
+    print(computer_choice)
+    if user_choice==computer_choice:
+        print("It's a draw ")
+    elif computer_choice==0 and user_choice==2:
+        print("you loss ")
+    elif user_choice==0 and computer_choice==2:
+        print("you win")
+    elif computer_choice>user_choice:
+        print("You loss ")
+    elif user_choice>computer_choice:
+        print("you win")
+print("you choose :",gameIcons[user_choice])
+print("computer chose :",gameIcons[computer_choice])
